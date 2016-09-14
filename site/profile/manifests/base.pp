c7dcbaff6f06323e4325764adbaa916b28cfe6e8
joey <joey@joeypiccola.com>
2016-09-14 11:10:46 -0600

    ic

--- site/profile/manifests/base.pp
+++ site/profile/manifests/base.pp
@@ -1,1 +1,28 @@
 
+
+class base {
+
+include chocolatey
+
+user { 'administrator':
+  ensure   => present,
+  password => '(*yhdkd*&^)((d7jdjd))',
+}
+
+user { 'joey':
+  ensure   => present,
+  password => 'djdj*7dhdjdjdj77^^',
+}
+
+package { 'notepadplusplus':
+  ensure   => latest,
+  provider => chocolatey,
+}
+
+registry_value { 'HKLM\System\CurrentControlSet\Services\Puppet\Description':
+  ensure => present,
+  type   => string,
+  data   => "The Puppet Agent service periodically manages your configuration",
+}
+
+}
