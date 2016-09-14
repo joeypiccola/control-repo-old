196ffa1911c5217582f34255a0ee16db8f372cc1
joeypiccola <joey.piccola@gmail.com>
2016-09-14 15:10:16 -0600

    Create datacenter.rb

---
+++ site/profile/lib/facter/datacenter.rb
@@ -0,0 +1,16 @@
+# datacenter.rb
+Factor.add('datacenter') do
+  setcode do
+    hostname = Facter.value(:hostname)
+    case hostname 
+    when /app/
+      'appserver'
+    when /web/
+      'webserver'
+    when /db/
+      'dbserver'
+    else 
+      'nodc'
+    end
+  end
+end
